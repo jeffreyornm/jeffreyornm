b78096f83c0d231bedf40a8db76e5a033765ce8d
Jeffrey Klein <JeffreyjkKlein@gmail.com>
2020-05-21 16:36:10 -0600

    working on jquery stuff

---
+++ public_html/script.js
@@ -0,0 +1,49 @@
+$(document).ready(function () {
+  /
+
+  $("#contact").validate({
+    debug: true,
+    errorClass: "alert alert-danger",
+    errorLabelContainer: "#output-area",
+    errorElement: "div",
+
+    rules: {
+      name: {
+        required:true
+      },
+      email: {
+        email: true,
+        required:true
+      },
+      message: {
+        required:true,
+        maxLength:2000
+      }
+    },
+
+    messages: {
+      name: {
+        required: "Name is a required field"
+      },
+      email: {
+        required: "Email is a required field",
+        email: "Invalid Email"
+      },
+      message: {
+        required: "Message is required",
+        maxLength: "Message is too long"
+      }
+    },
+
+    submitHandler: function (form) {
+      $("#contact").ajaxSubmit({
+        type: "POST",
+        url: $("#contact").attr("action"),
+        success: function (ajaxOutput) {
+
+        }
+      })
+
+    }
+  })
+})
