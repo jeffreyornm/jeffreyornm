aaa2fbeacc2588993a4e87f19e808fdf3e29b241
Jeffrey Klein <JeffreyjkKlein@gmail.com>
2020-05-28 17:45:01 -0600

    added node modules, index.js, package.json and package-lock.json into src directory. Fixed issues with contact form name attributes and script links

--- public_html/script.js
+++ public_html/script.js
@@ -1,37 +1,50 @@
 $(document).ready(function () {
-  /
+  /**
+   * jQuery Validate Function
+   *
+   * This function provides front-end validation for your form.
+   * If all tests set up here pass, the form data is AJAX submitted
+   * to the mailer.php file.
+   *
+   * Update this file as needed for your form.
+   * All ids and name values must match up to your form here.
+   *
+   * @author Rochelle Lewis <rlewis37@cnm.edu>
+   **/
 
   $("#contact").validate({
     debug: true,
     errorClass: "alert alert-danger",
     errorLabelContainer: "#output-area",
     errorElement: "div",
-
+    // rules here define what is good or bad input
+    //each rule starts with the form input element's NAME attribute
     rules: {
       name: {
         required:true
       },
       email: {
         email: true,
-        required:true
+        required: true
       },
       message: {
-        required:true,
-        maxLength:2000
+        required: true,
+        maxlength:2000
       }
     },
+    //error message to display to the end user when rules above dont pass
 
     messages: {
       name: {
-        required: "Name is a required field"
+        required: "Name is required field"
       },
       email: {
         required: "Email is a required field",
         email: "Invalid Email"
       },
       message: {
-        required: "Message is required",
-        maxLength: "Message is too long"
+        required: "message is required",
+        maxlength: "Message is too long"
       }
     },
 
@@ -48,9 +61,11 @@ $(document).ready(function () {
             $("#contact")[0].reset();
           }
 
+
         }
       })
 
     }
   })
+
 })
